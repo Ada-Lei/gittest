905f28b31d8a8f3eb965943360eb3b912d0e0123
Rei <Rei@email.com>
2024-09-12 09:50:09 +0800

    subtraction

--- test2.java
+++ test2.java
@@ -3,11 +3,10 @@ public class test2{
     public static void (main String []Args){
         int a: 5;
         int b: 3;
-
-        System.out.println("Addition" + addition());
         
-        class addition(int a, int b){
-            return a + b;
+        static void addition(){
+            int sum= a + b;
+            System.out.println("Addition:" + sum);
         }
 
     }
